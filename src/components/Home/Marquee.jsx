8236fec93b76158b67f92cdeded53483929e3eb9
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-03-21 22:42:32 +0630

    marquee text border removed

--- src/components/Home/Marquee.jsx
+++ src/components/Home/Marquee.jsx
@@ -7,7 +7,7 @@ const Marquee = () => {
   // console.log(bannerText.text);
   return (
     <div>
-      <marquee className='p-2 shadow-lg rounded  text-light py-1 px-2 m-2 border border-warning '>
+      <marquee className='p-2 shadow-lg rounded  text-light py-1 px-2 m-2'>
         {bannerText && bannerText.text}
         {!bannerText && 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis tempora cupiditate quaerat pariatur consequuntur libero a, necessitatibus rerum nemo eius voluptatibus! Voluptatum ducimus dolore alias laboriosam, accusamus harum vitae.'}
       </marquee>
